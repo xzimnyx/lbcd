0dbbbaaeeb380a2e368e64814d2ebd3dbcaba66f
Roy Lee <roylee17@gmail.com>
2021-08-03 22:57:36 -0700

    [lbry] config: support 'clmtimpl' and 'clmtheight' flags

--- doc.go
+++ doc.go
@@ -45,6 +45,8 @@ Application Options:
                               50000)
       --blocksonly            Do not accept transactions from remote peers.
   -C, --configfile=           Path to configuration file
+	    --clmtimpl=             Implementation of ClaimTrie
+	    --clmtheight=           Reset height of ClaimTrie
       --connect=              Connect only to the specified peers at startup
       --cpuprofile=           Write CPU profile to the specified file
   -b, --datadir=              Directory to store data
