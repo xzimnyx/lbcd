3910319e80a5d64b48ce3c07d2cd7d71c1fc926d
Roy Lee <roylee17@gmail.com>
2021-09-14 11:40:34 -0700

    [lbry] ci: separate test and lint

--- goclean.sh
+++ goclean.sh
@@ -9,8 +9,5 @@
 
 set -ex
 
-# Automatic checks
-golangci-lint run
-
 env GORACE="halt_on_error=1" go test -race -tags="rpctest" -covermode atomic -coverprofile=profile.cov ./...
 go test -bench=. -benchtime=4000x ./claimtrie/
